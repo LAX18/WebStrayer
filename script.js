52b02d0a44ae55f7ae964f9805aea3af7cd7680e
LAX18 <gaminglax18@gmail.com>
2019-01-13 17:32:49 +0000

    New attempt

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changePDF(chapter) {
   var container = document.getElementById("pdfcontainer")
-  container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf' type='application/pdf' height='100%' width='100%'></embed>"
+  container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf' type='application/pdf' style='height:calc(100vh - 70px)' width='100%'></embed>"
 }
