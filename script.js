a17c5a7adfb8ecb9c9ec53ee17216183360927b4
LAX18 <gaminglax18@gmail.com>
2019-01-13 17:10:12 +0000

    HTTPS not HTTP

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changePDF(chapter) {
   var container = document.getElementById("pdfcontainer")
-  container.innerHTML = "<embed src='http://lax18.github.io/StrayerChapters/"+chapter+".pdf' type='application/pdf'></embed>"
+  container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf' type='application/pdf'></embed>"
 }
