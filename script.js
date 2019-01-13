58c2daefe924fb5651cfd45a414804fa20e60dd0
LAX18 <gaminglax18@gmail.com>
2019-01-13 02:40:29 +0000

    Dumbass...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changePDF(chapter) {
   var container = document.getElementById("pdfcontainer")
-  container.innerHTML = "<embed src='http://lax18.github.io/StrayerChapters/"+chapter+".pdf'></embed>"
+  container.innerHTML = "<embed src='http://lax18.github.io/StrayerChapters/"+chapter+".pdf' type='application/pdf'></embed>"
 }
