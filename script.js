abf479ac3bce3314b9a5a1fff3e3a80d2f1e05a8
LAX18 <34464552+LAX18@users.noreply.github.com>
2019-03-28 12:54:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changePDF(chapter) {
   var container = document.getElementById("pdfcontainer")
-  container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf#toolbar=0&navpanes=0&scrollbar=0' type='application/pdf' style='height:calc(100vh - 70px)' width='100%'></embed>"
+  container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf#toolbar=0&navpanes=0' type='application/pdf' style='height:calc(100vh - 70px)' width='100%'></embed>"
   document.getElementById("chapter_name").innerHTML = chapter
 }
