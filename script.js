774f62aae5d07c47e7b867a1e5a1dc2291f28995
LAX18 <34464552+LAX18@users.noreply.github.com>
2019-04-25 18:00:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function changePDF(chapter) {
   container.innerHTML = "<embed src='https://lax18.github.io/StrayerChapters/"+chapter+".pdf#toolbar=1&navpanes=0' type='application/pdf' style='height:calc(100vh - 70px)' width='100%'></embed>"
   document.getElementById("chapter_name").innerHTML = chapter
 }
+function onLoad() {
+  if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope:',  registration.scope);
+  }).catch(function(error) {
+    console.log('ServiceWorker registration failed:', error);
+  });
+}
+}
